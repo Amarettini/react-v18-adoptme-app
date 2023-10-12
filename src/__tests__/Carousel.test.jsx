b7876b11fbd93eb670aa9e335a68b6a55a6bb769
Patrik Pyrzanowski <patrik.pyrzanowski@gmail.com>
2023-10-12 18:10:37 +0200

    Add tests for Carousel component

---
+++ src/__tests__/Carousel.test.jsx
@@ -0,0 +1,34 @@
+import { expect, test } from "vitest";
+import { render } from "@testing-library/react";
+import Carousel from "../Carousel";
+
+test("lets users click on thumbnails to make them the hero img", async () => {
+  const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
+  const carousel = render(<Carousel images={images} />);
+
+  const hero = await carousel.findByTestId("hero");
+  expect(hero.getAttribute("src")).toContain(images[0]);
+
+  for (let i = 0; i < images.length; i++) {
+    const image = images[i];
+    const targetThumb = await carousel.findByTestId(`thumbnail${i}`);
+
+    await targetThumb.click();
+
+    // hero image should be same as target thumbnail
+    expect(hero.getAttribute("src")).toContain(image);
+    // target thumbnail should have active class
+    expect(Array.from(targetThumb.classList)).toContain("active");
+
+    const thumbnails = await carousel.findAllByTestId(/thumbnail/);
+    const nonActiveThumbs = thumbnails.filter((thumbnail) => !(thumbnail === targetThumb));
+    console.log(nonActiveThumbs.length);
+
+    // non-target thumbnails should not have active class
+    expect(nonActiveThumbs.map((thumb) => thumb.classList.contains("active"))).toEqual([
+      false,
+      false,
+      false,
+    ]);
+  }
+});
